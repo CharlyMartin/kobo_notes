2c97d5abe7075a839ebd8b0dd0273ea9b0381120
Charly <charly.martin.1207@gmail.com>
2017-12-25 17:07:06 +1100

    Adding date in note's title to sort it

--- main.rb
+++ main.rb
@@ -8,7 +8,7 @@ DB.results_as_hash = true
 
 # Extracting Text, Bookid and date from the
 rows = DB.execute('
-  SELECT Text, ContentID FROM Bookmark
+  SELECT Text, VolumeID, DateCreated FROM Bookmark
   ORDER BY DateCreated ASC
 ')
 
@@ -18,16 +18,19 @@ book_keys = extract_keys(rows);
 
 
 # looping over the book keys
-book_keys.each do |key|
+book_keys.each do |book_key|
 
   # creating an object for the current key
   data = {
-    "#{key}" => []
+    key: book_key,
+    text: [],
+    date: nil
   }
 
   # populating the object
   rows.each do |hash|
-    data[key] << hash["Text"] if (hash["ContentID"].split('!')[0] == key)
+    data[:text] << hash["Text"] if (hash["VolumeID"] == book_key)
+    data[:date] = hash["DateCreated"] if (hash["VolumeID"] == book_key)
   end
 
   save_as_txt(data)
--- methods.rb
+++ methods.rb
@@ -1,11 +1,12 @@
 def extract_keys(array)
-  array.map {|item| item[1].split('!')[0] }.uniq
+  array.map {|item| item[1] }.uniq
 end
 
-def save_as_txt(hash)
-  key = hash.keys.first
-  text = hash[key].join("\n")
-  file_path = "output/#{key}.txt"
+def save_as_txt(obj)
+  key = obj[:key]
+  text = obj[:text].join("\n")
+  date = obj[:date]
+  file_path = "output/#{date} - #{key}.txt"
 
   File.open(file_path, "w") do |file|
     file.write(text)
